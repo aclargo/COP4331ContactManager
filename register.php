65648fd4b54098f7a792e701b95beea1a1cdffbf
ivanp589 <55461253+ivanp589@users.noreply.github.com>
2022-06-14 13:30:15 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
 	$inData = getRequestInfo();
 	
+	// $id = 0;
 	$firstName = trim($inData["FirstName"]);
 	$lastName = trim($inData["LastName"]);
 	$login = trim($inData["login"]);
@@ -15,37 +16,50 @@
 	//if it connects to the server we can start filling out the text fields
 	else
 	{
-		//need to add an elseif to check if the username is already taken.
-// 		$sql_u = "SELECT * FROM Users WHERE Login='$login'";
-// 		$res_u = mysqli_query($conn, $sql_u);
-		
 		// need to add somewhere in here code to add user to the database.
 
 		//check if all fields are full
-		if(
-			empty($firstName)
-			||empty($lastName)
-			||empty($login)
-			||empty($password)
-			)
-		{
-			returnWithError("Please fill in all fields");
-		}
+		// if(
+		// 	empty($firstName)
+		// 	||empty($lastName)
+		// 	||empty($login)
+		// 	||empty($password)
+		// 	)
+		// {
+		// 	returnWithError("Please fill in all fields");
+		// }
 		
-		else 
-		{
+		// else 
+		// {
 			//add the user to the users table here
-			$query = "INSERT INTO `Users` (FirstName,LastName,Login, password) 
-      	   	VALUES ('$firstName','$lastName','$login', '".md5($password)."')";
 
+			
+			$query = "INSERT INTO `Users` (FirstName,LastName,Login, Password) 
+      	   	VALUES ('$firstName','$lastName','$login', '".md5($password)."')";
            	$results = mysqli_query($conn, $query);
-           	$query = "INSERT INTO `Users` (FirstName,LastName,Login, password) 
+
+			
+           	$query = "INSERT INTO `Users` (FirstName,LastName,Login, Password) 
       	   	VALUES ('$firstName','$lastName','$login', '$password')";
       	   	$results = mysqli_query($conn, $query);
-           	echo 'Saved!';
-           	// returnWithError("Something isnt wrong");
+
+
+      	   	$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+			$stmt->bind_param("ss", $login, $password);
+			$stmt->execute();
+			$result = $stmt->get_result();
+
+      	   	if ($row = $result->fetch_assoc()  )
+      	   	{
+				$_SESSION['user'] = $row['ID'];
+				returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'], "Saved!");
+			}
+
+
+           	// echo '"Saved!"';
+           	// returnWithError("Saved!");
 			exit();
-		}
+		// }
 
 	}
 
@@ -66,7 +80,7 @@
 		sendResultInfoAsJson( $retValue );
 	}
 
-	function returnWithInfo( $firstName, $lastName, $id )
+	function returnWithInfo( $firstName, $lastName, $id ,$err)
 	{
 		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
 		sendResultInfoAsJson( $retValue );
