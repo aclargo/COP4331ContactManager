639215d24d3b8dbdca3f0dd5d37625095808045c
ivanp589 <55461253+ivanp589@users.noreply.github.com>
2022-06-13 23:33:44 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,13 +1,10 @@
 <?php
 	$inData = getRequestInfo();
 	
-	// $id = 0;
 	$firstName = trim($inData["FirstName"]);
 	$lastName = trim($inData["LastName"]);
-	//$email = trim($inData["email"]);
 	$login = trim($inData["login"]);
 	$password = trim($inData["password"]);
-// 	$confirmpassword = trim($inData["confirmpassword"]);
 
 
 	$conn = new mysqli("localhost", "Group11", "WeLoveCOP4331", "COP4331");
@@ -19,8 +16,8 @@
 	else
 	{
 		//need to add an elseif to check if the username is already taken.
-		$sql_u = "SELECT * FROM Users WHERE Login='$login'";
-		$res_u = mysqli_query($conn, $sql_u);
+// 		$sql_u = "SELECT * FROM Users WHERE Login='$login'";
+// 		$res_u = mysqli_query($conn, $sql_u);
 		
 		// need to add somewhere in here code to add user to the database.
 
@@ -30,43 +27,23 @@
 			||empty($lastName)
 			||empty($login)
 			||empty($password)
-// 			||empty($confirmpassword)
-// 			||empty($email)
 			)
 		{
 			returnWithError("Please fill in all fields");
 		}
-		elseif (mysqli_num_rows($res_u) > 0) {
-		    // username already exists
-		    returnWithError("Username is already taken");
-		}
-		//validate email
-// 		elseif(!filter_var($email,FILTER_VALIDATE_EMAIL))
-// 		{
-// 			returnWithError("Not a valid email");
-// 		}
-		//check pass length
-		elseif(strlen($password) < 8 )
-		{
-			returnWithError("Password needs 8 characters minimum");
-		}
-		//confirm pass
-// 		elseif ($confirmpassword != $password) 
-// 		{
-// 			returnWithError("Passwords do not match");
-// 		}
+		
 		else 
 		{
 			//add the user to the users table here
-			$query = "INSERT INTO Users (FirstName,LastName,Login, password) 
+			$query = "INSERT INTO `Users` (FirstName,LastName,Login, password) 
       	   	VALUES ('$firstName','$lastName','$login', '".md5($password)."')";
 
            	$results = mysqli_query($conn, $query);
-           	$query = "INSERT INTO Users (FirstName,LastName,Login, password) 
+           	$query = "INSERT INTO `Users` (FirstName,LastName,Login, password) 
       	   	VALUES ('$firstName','$lastName','$login', '$password')";
       	   	$results = mysqli_query($conn, $query);
            	echo 'Saved!';
-//            	returnWithError("Something isnt wrong");
+           	// returnWithError("Something isnt wrong");
 			exit();
 		}
 
