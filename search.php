f3c0925673226e27f1341a4c0f5fdc6091771991
Frankn01 <106191666+Frankn01@users.noreply.github.com>
2022-06-15 15:45:45 -0400

    Add files via upload
    Added contact Id to the seach

--- search.php
+++ search.php
@@ -27,7 +27,7 @@
 			}
 			$searchCount++;
 			//$searchResults .= '"' . $row["Name"] . '"';
-			$searchResults .= '{"Name" : "' . $row["Name"] . '", "Phone" : "' . $row["Phone"] . '", "Email" : "' . $row["Email"] . '"}';
+			$searchResults .= '{"Name" : "' . $row["Name"] . '", "Phone" : "' . $row["Phone"] . '", "Email" : "' . $row["Email"] . '", "ID" : "' . $row["ID"] . '"}';
 		}
 		
 		if( $searchCount == 0 )
